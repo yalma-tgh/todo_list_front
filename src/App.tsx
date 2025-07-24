02e3b2ea26795f6826154e4abe219d23f176ba20
DanyalTaghipor <danyqw.dt@gmail.com>
2025-07-24 19:01:04 +0330

    remove cookie and keep token based auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Login from "./components/login";
 import Register from "./components/register";
 import Home from "./components/home";
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 };
 
