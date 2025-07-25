d465785cb314f1034fb8ff4c5f2313b51632b154
DanyalTaghipor <danyqw.dt@gmail.com>
2025-07-25 13:44:26 +0330

    modify config

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,11 @@
-import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import tempo from "tempo-devtools/vite";
+import path from "path";
+import { tempo } from "tempo-devtools/dist/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "development" ? "/" : process.env.VITE_BASE_PATH || "/",
-  optimizeDeps: {
-    entries: ["src/main.tsx", "src/tempobook/**/*"],
-  },
   plugins: [react(), tempo()],
-  resolve: {
-    preserveSymlinks: true,
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
   server: {
     proxy: {
       "/api": {
@@ -24,7 +14,10 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
-    // @ts-ignore
-    allowedHosts: true,
-  }
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
